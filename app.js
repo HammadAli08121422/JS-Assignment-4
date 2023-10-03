c6ab4047f40d83279f50285d63fd2561c0b26bb4
unknown <hafizhammadali08121422@gmail.com>
2023-10-03 23:06:22 +0500

    complete 4th task

---
+++ app.js
@@ -0,0 +1,40 @@
+var var1 =  "3 variables"
+var variables2 = "3 variables"
+var variables3 = "3 variables"
+
+console.log(var1)
+console.log(variables2)
+console.log(variables3)
+
+
+
+var rules = "<h1>“Rules for naming JS variables”</h1> <br><br>"
+
+{document.write(rules)}
+
+var rule_1 = "Variable names can only contain numbers, $ and _."
+
+var rule_1_eg = " For example : $my_1stVariable <br>" 
+
+var $my_1stVariable = rule_1 + rule_1_eg 
+
+{document.write($my_1stVariable)}
+
+var rule_2 = "Variables must begin with a letter, $ or _."
+
+var rule_2_eg = "For example $name, _name or name<br>"
+
+var $name = rule_2 + rule_2_eg
+
+{document.write($name)}
+
+var rule_3 = "Variable names are case sensitive<br>"
+
+{document.write(rule_3)}
+
+var rule_4 = "Variable names should not be JS keywords<br>"
+
+{document.write(rule_4)}
+
+alert("Hey ! Here JS displayed in browser..")
+
